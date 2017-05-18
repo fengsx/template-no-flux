c5a6a061b945c6c411c4bfff04052263ac7e30a9
wenzhao.fw <wenzhao.fw@alibaba-inc.com>
2017-05-18 14:38:08 +0800

    remove dist folder

--- proj/webpack.config.js
+++ proj/webpack.config.js
@@ -11,25 +11,25 @@ module.exports = function (config) {
 
   if (process.argv[2] == 'server') {
     config.externals = [{
-        'lie': 'window.Promise',
-        'react': 'window.React',
-        'react-dom': 'window.ReactDOM || window.React',
-        'react-router': 'window.ReactRouter'
-      },
-      (context, request, callback, matches) => {
-        if (/uxcore\/assets\//.test(request)) {
-          callback(null, '0');
-        } else if (matches = /uxcore\/lib\/(\w+)/.exec(request)) {
-          callback(null, `window.Uxcore.${matches[1]}`);
-        } else if (matches = /react\-addons((\-\w+)+)/.exec(request)) {
-          const addon = matches[1].replace(/\-((\w)(\w+))/g, (p, p1, p2, p3) =>
-            (!/^(css|dom|umd)$/.test(p1) ? p2.toUpperCase() + p3 : p1.toUpperCase())
-          );
-          callback(null, `window.React.addons.${addon}`);
-        } else {
-          callback();
-        }
-      },
+      'lie': 'window.Promise',
+      'react': 'window.React',
+      'react-dom': 'window.ReactDOM || window.React',
+      'react-router': 'window.ReactRouter'
+    },
+    (context, request, callback, matches) => {
+      if (/uxcore\/assets\//.test(request)) {
+        callback(null, '0');
+      } else if (matches = /uxcore\/lib\/(\w+)/.exec(request)) {
+        callback(null, `window.Uxcore.${matches[1]}`);
+      } else if (matches = /react\-addons((\-\w+)+)/.exec(request)) {
+        const addon = matches[1].replace(/\-((\w)(\w+))/g, (p, p1, p2, p3) =>
+          (!/^(css|dom|umd)$/.test(p1) ? p2.toUpperCase() + p3 : p1.toUpperCase())
+        );
+        callback(null, `window.React.addons.${addon}`);
+      } else {
+        callback();
+      }
+    },
     ];
   } else {
     delete config.externals;
